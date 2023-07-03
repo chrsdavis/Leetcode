06fe04cb1293d3ac5e16d0cd0ed0a81fa2647276
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2023-07-02 23:19:14 -0700

    Documentation for 875

--- 875-Koko-Eating-Bananas/lambda.cpp
+++ 875-Koko-Eating-Bananas/lambda.cpp
@@ -6,25 +6,29 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int upper = *max_element(piles.begin(), piles.end());
         int lower = 1;
-        int min_time = -1;
+        int min_amount = -1; // best time
         while(lower <= upper) {
-            int mid = (upper + lower) >> 1;
-            unsigned int current_time = 0;
+            int mid = (upper + lower) >> 1; // # of bananas to eat per cycle
+            unsigned int current_time = 0; // # of cycles total
             for_each(piles.begin(), piles.end(), [=,&current_time](auto p) {
-                current_time += (p % mid) ? 1 + p/mid : p/mid;
+                current_time += (p % mid) ? 1 + p/mid : p/mid; // number of cycles required
             });
-            if(current_time > h)
+            if(current_time > h)  // taking too long, so need to increase count
                 lower = mid + 1;
-            else {
-                min_time = mid;
+            else { // this answer works, so try reducing the count
+                min_amount = mid;
                 upper = mid - 1;
             }
         }
-        return min_time;
+        return min_amount;
     }
 };
 
-
+/*
+For lower, we need to eat at least 1 banana, as otherwise we would never finish.
+For upper, just start by taking the largest banana pile, since that means we are guaranteed to finish within
+size(piles) steps.
+*/
 
 /* 
 For beter performance, add a flag and a break once the current_time > h, 
