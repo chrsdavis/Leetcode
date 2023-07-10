210621228ec1a11c50f656e64095e3236cf30d2a
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2023-07-10 16:52:45 -0700

    Another stackless solution

--- 020-Valid-Parenthesis/stackless.cpp
+++ 020-Valid-Parenthesis/stackless.cpp
@@ -2,6 +2,8 @@
 
 // Uses the string s as a stack for no extra memory
 
+// correct matchings should have an ASCII literal difference of either 1 or 2
+// we don't have to worry about the 0 case (']]') bc we don't push closing parens on stack
 
 class Solution {
 public:
@@ -10,7 +12,7 @@ public:
         for (char c : s)
             if (c % 26 % 3) // if opening parenthesis
                 s[n++] = c; // push to stack
-            else if (!n || c%s[--n]>2) // if no matching opening in stack, also pop stack
+            else if (!n || c % s[--n] > 2) // if no matching opening in stack, also pop stack
                 return false;
         return !n; // n == 0 means stack is empty
     }
