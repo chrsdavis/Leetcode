3fa4e651ba69f2aeb1266a7b29f96f6273dd4ab0
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2023-08-17 22:12:57 -0400

    100

--- util/common.h
+++ util/common.h
@@ -25,4 +25,14 @@ using namespace std;
 #include <utility>
 #include <vector>
 
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 #endif
