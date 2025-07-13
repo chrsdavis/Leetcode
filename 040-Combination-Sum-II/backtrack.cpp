1fae93aa19e2b247a6cae2fbc4c8dae5d7b3c460
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-07-12 18:47:31 -0700

    40

---
+++ 040-Combination-Sum-II/backtrack.cpp
@@ -0,0 +1,50 @@
+// Similar to Combination Sum (39), except I have to check for duplicate answers now
+// candidates to sum up to target are not unique, and I can only use each one once.
+// Cannot use duplicate answers
+// Now, I explicitly check if an answer was a duplicate (I might be able to optimize this away...?)
+// Since I add all answers in sorted order (descending), the answer is in descending lexicographic order
+// Can check for duplicate in O(log n) with no additional space for hash map
+
+// I also skip adding the same element to do early pruning of duplicate solutions
+// E.g. ..., a, n, n, n, n, n, n, c
+// I get to the first n, backtrack it, then skip all subsequent n's and go straight to c
+
+class Solution {
+private:
+    vector<int> nums;
+    vector<vector<int>> ans;
+    vector<int> cur;
+
+    void backtrack(int target, int idx) {
+        if (target == 0) {
+            auto sol = std::lower_bound(ans.begin(), ans.end(),
+                                        cur, std::greater{});
+            if (sol == ans.end() || *sol != cur) // check for duplicate answer
+                ans.emplace_back(cur);
+            return;
+        } else if (target < 0) {
+            return;
+        }
+
+        auto it = std::lower_bound(nums.begin() + idx, nums.end(),
+                                   target, std::greater{});
+        int prev = -1;
+        for (int i = it - nums.begin(); i < nums.size(); i++) {
+            if (nums[i] == prev) continue; // skip duplicates
+            prev = nums[i];
+            cur.push_back(nums[i]);
+            backtrack(target - nums[i], i+1);
+            cur.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end(), std::greater{});
+        swap(nums, candidates);
+        ans.clear();
+        cur.clear();
+        backtrack(target, 0);
+        return ans;
+    }
+};
